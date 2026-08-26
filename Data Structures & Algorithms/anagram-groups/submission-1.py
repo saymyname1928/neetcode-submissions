cfaf1ec5c621edc0d53d044fff5b03af38fa0f10
saymyname1928 <ohnohbubu@gmail.com>
2026-08-26 09:28:22 +0900

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,14 @@
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        anagrams = defaultdict(list)
+
+        for word in strs:
+            word_sorted = "".join(sorted(word))
+            anagrams[word_sorted].append(word)
+            
+        return list(anagrams.values())
+            
+
+        
