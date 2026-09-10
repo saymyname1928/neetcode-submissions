fcb89bc2e941263a31444a1182c4cfc2ad2e9944
saymyname1928 <ohnohbubu@gmail.com>
2026-09-10 15:36:32 +0900

    Add: find-minimum-in-rotated-sorted-array - submission-2

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+
+    # if we cut array into half, one partition is sorted and the other is not. 
+    def findMin(self, nums: List[int]) -> int:
+        
+        # l, r = 0, len(nums) - 1
+        # while l < r:
+        #     mid = (l + r) // 2
+        #     if nums[mid] > nums[r]:
+        #         l = mid + 1
+        #     else:
+        #         r = mid
+        # return nums[l]
+
+        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = (l + r) // 2
+            if nums[mid] > nums[r]:
+                l = mid + 1
+            else:
+                r = mid
+        return nums[l]
